6db8695a2191a972cf0b37cb5f5450523bfb5bf2
vetri79 <s.vetrivelsampathkumar@gmail.com>
2025-08-01 20:50:11 +0530

    Update userRoutes.js

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,1 +1,22 @@
+// File: src/routes/userRoutes.js
+import express from 'express';
+import UserController from '../controllers/userController.js';
 
+const router = express.Router();
+const userController = new UserController();
+
+const setUserRoutes = (app) => {
+    app.use('/api/users', router);
+
+    router.post('/', userController.createUser);
+    router.post('/login', userController.signIn);
+    router.post('/send-otp', userController.sendOtp);
+    router.post('/verify-otp', userController.verifyOtp);
+    router.post('/reset-password', userController.resetPassword);
+
+    router.get('/:id', userController.getUser);
+    router.put('/:id', userController.updateUser);
+    router.delete('/:id', userController.deleteUser);
+};
+
+export default setUserRoutes;
